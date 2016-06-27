7d4c6b55d4bc8377d69181f44011b26450ce0937
Lennart0 <lennart0@hotmail.se>
2016-06-27 15:16:14 +0200

    Module Schedule (not tested, step 1)

---
+++ LMS/LMS/Controllers/ScheduleController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LMS.DataAccessLayer;
+using LMS.Models;
+
+namespace LMS.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Schedule
+        public ActionResult Index( Guid? id = null )
+        {
+            Course course = db.Users.SingleOrDefault( u => u.UserName == User.Identity.Name )?.Course;
+
+            if ( course == null && id != null )
+                course = db.Courses.SingleOrDefault( c => c.Id == id.Value );
+
+            if (course == null) {
+                return Content( "INGEN KURS VALD!" );
+            }
+
+            return View(course.Modules);
+        }
+
+        // GET: Schedule/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // GET: Schedule/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedule/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,Name,Start,End")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                module.Id = Guid.NewGuid();
+                db.Modules.Add(module);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(module);
+        }
+
+        // GET: Schedule/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // POST: Schedule/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,Name,Start,End")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(module).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(module);
+        }
+
+        // GET: Schedule/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // POST: Schedule/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Module module = db.Modules.Find(id);
+            db.Modules.Remove(module);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
