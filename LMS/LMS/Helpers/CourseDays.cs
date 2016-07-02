1deb0cb5d1e461f936eaf649d329861606df077d
L <L@DESKTOP-G0K080U>
2016-07-02 12:57:24 +0200

    CourseDays rewritten...

--- LMS/LMS/Helpers/CourseDays.cs
+++ LMS/LMS/Helpers/CourseDays.cs
@@ -8,35 +8,65 @@ namespace LMS.Helpers
     public class CourseDays
     {
         /// <summary>
-        /// Calculates course days from(incl) first date to(incl) last date, depending on weekdays and holidays
+        /// Calculates course days from(incl) first date with no limit, depending on course weekdays and holidays
+        /// </summary>
+        /// <param name="firstDate">First day of course</param>
+        /// <param name="courseWeekdays">Weekdays of course days (default monday - friday)</param>
+        public CourseDays(DateTime firstDate, params DayOfWeek[] courseWeekdays) {
+            this.firstDate = firstDate;
+            this.lastDate = firstDate;
+            noEnd = true;
+            this.courseWeekdays = courseWeekdays;
+            if (this.courseWeekdays.Length == 0)
+                this.courseWeekdays = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+        }
+
+        /// <summary>
+        /// Calculates course days from(incl) first date to(incl) last date, depending on course weekdays and holidays
         /// </summary>
         /// <param name="firstDate"></param>
         /// <param name="lastDate"></param>
-        /// <param name="weekdays"></param>
-        public CourseDays( DateTime firstDate, DateTime lastDate, params DayOfWeek[] weekdays ) {
-            this.firstDate = firstDate;
+        /// <param name="courseWeekdays"></param>
+        public CourseDays( DateTime firstDate, DateTime lastDate, params DayOfWeek[] courseWeekdays ) : this(firstDate, courseWeekdays) {            
             this.lastDate = lastDate;
-            this.weekdays = weekdays;
-            if ( this.weekdays.Length == 0 )
-                this.weekdays = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+            noEnd = false;
         }
 
+        /// <summary>
+        /// List of all course days. (Only usefull if last date is specified)
+        /// </summary>
         public List<DateTime> Days {
             get {
-                if ( courseDays == null )
+                if (courseDays == null)
                     CalcCourseDays();
                 return courseDays;
             }
         }
+
+        /// <summary>
+        /// Is the day a course day?
+        /// </summary>
+        /// <param name="day">The day to test.</param>
+        /// <returns>True if day is course day.</returns>
         public bool IsCourseDay( DateTime day ) {
+            DateTime d = day.Date;
+            if (noEnd)
+                return d >= firstDate && IsCourseDay_NoLimits(d);
+
             if ( courseDays == null )
                 CalcCourseDays();
-            return courseDays.IndexOf( day.Date ) >= 0;
+            return courseDays.IndexOf( d.Date ) >= 0;            
         }
+
+        /// <summary>
+        /// Next course day after preDay, or null if none.
+        /// </summary>
+        /// <param name="preDay">The previous day.</param>
+        /// <returns>Next course day after preDay, or null if none.</returns>
         public DateTime? NextCourseDay( DateTime preDay ) {
             DateTime day = preDay.Date;
-            while (lastDate < firstDate || day <= lastDate) {
-                day += new TimeSpan( 1, 0, 0, 0 );
+            while (noEnd || day <= lastDate) {
+                day = AddDays(day, 1);
                 if ( IsCourseDay( day ) )
                     return day;
             }
@@ -44,107 +74,94 @@ namespace LMS.Helpers
         }
 
 
-        struct Date
-        {
-            public int month;
-            public int day;
-        }
-
         private List<DateTime> courseDays = null;
-        private DayOfWeek[] weekdays;
-        private bool excludeHolidays;
+        private DayOfWeek[] courseWeekdays;
+        //private bool excludeHolidays;
         private DateTime firstDate;
         private DateTime lastDate;
+        private bool noEnd;
 
-        Dictionary<int, Date> easterSunday = new Dictionary<int, Date>() {
-            { 2015, new Date {month = 4, day = 5 } },
-            { 2016, new Date {month = 3, day = 27 } },
-            { 2017, new Date {month = 4, day = 16 } },
-            { 2018, new Date {month = 4, day = 1 } },
-            { 2019, new Date {month = 4, day = 21 } },
-            { 2020, new Date {month = 4, day = 12 } },
-            { 2021, new Date {month = 4, day = 4 } },
-            { 2022, new Date {month = 4, day = 17 } },
-            { 2023, new Date {month = 4, day = 9 } },
-            { 2024, new Date {month = 3, day = 31 } },
-            { 2025, new Date {month = 4, day = 20 } },
+        static private Dictionary<int, DateTime> easterFriday = new Dictionary<int, DateTime>() {
+            { 2015, new DateTime(2015, 4,3 ) },
+            { 2016, new DateTime(2016, 3, 25 ) },
+            { 2017, new DateTime(2017, 4, 14 ) },
+            { 2018, new DateTime(2018, 3, 30 ) },
+            { 2019, new DateTime(2019, 4, 19 ) },
+            { 2020, new DateTime(2020, 4, 10 ) },
+            { 2021, new DateTime(2021, 4, 2 ) },
+            { 2022, new DateTime(2022, 4, 15 ) },
+            { 2023, new DateTime(2023, 4, 7 ) },
+            { 2024, new DateTime(2024, 3, 29 ) },
+            { 2025, new DateTime(2025, 4, 18 ) },
         };
 
-        HashSet<Date> fixedHolidays = new HashSet<Date>() {
-            new Date { month = 1, day = 1 },
-            new Date { month = 1, day = 6 },
-            new Date { month = 5, day = 1 },
-            new Date { month = 6, day = 6 },
-            new Date { month = 12, day = 24 },
-            new Date { month = 12, day = 25 },
-            new Date { month = 12, day = 26 },
-            new Date { month = 12, day = 31 },
-        };
+        private Dictionary<int, HashSet<DateTime>> holidays = new Dictionary<int, HashSet<DateTime>>();
 
-        Dictionary<int, HashSet<Date>> movingHolidays = new Dictionary<int, HashSet<Date>>();
+        private void CalcHolidays(int year) {
+            var set = new HashSet<DateTime>();
 
-        private void CalcMovingHolidays(int year) {
-            var set = new HashSet<Date>();
-
-            if ( easterSunday.ContainsKey(year) ) {
-                Date es = easterSunday[year];
-                DateTime esDate = new DateTime( year, es.month, es.day ).Date;
-                DateTime esDateM2 = esDate - new TimeSpan( 2, 0, 0, 0 );
-                DateTime esDateM1 = esDate - new TimeSpan( 1, 0, 0, 0 );
-                DateTime esDateP1 = esDate + new TimeSpan( 1, 0, 0, 0 );
-                DateTime esDateP39 = esDate + new TimeSpan( 39, 0, 0, 0 );
-
-                set.Add( es );
-                set.Add( new Date { month = esDateM2.Month, day = esDateM2.Day } );
-                set.Add( new Date { month = esDateM1.Month, day = esDateM1.Day } );
-                set.Add( new Date { month = esDateP1.Month, day = esDateP1.Day } );
-                set.Add( new Date { month = esDateP39.Month, day = esDateP39.Day } );
+            //Easter + kristi himelsf.
+            if (easterFriday.ContainsKey(year)) {
+                DateTime ef_0 = easterFriday[year];
+                set.Add(ef_0);
+                set.Add(AddDays(ef_0, 1));
+                set.Add(AddDays(ef_0, 2));
+                set.Add(AddDays(ef_0, 3));
+                set.Add(AddDays(ef_0, 41)); //Kristi himelsf.
             }
 
-            var midsummer = new DateTime( year, 6, 19 ).Date;
-            int wdDiff = (int)DayOfWeek.Friday -( int)midsummer.DayOfWeek;
-            if ( wdDiff < 0 )
+            //Midsummer
+            var midsummer = new DateTime(year, 6, 19);
+            int wdDiff = (int)DayOfWeek.Friday - (int)midsummer.DayOfWeek;
+            if (wdDiff < 0)
                 wdDiff += 7;
-            midsummer += new TimeSpan( wdDiff, 0, 0, 0 );
-            set.Add( new Date { month = midsummer.Month, day = midsummer.Day } );
-            midsummer += new TimeSpan( 1, 0, 0, 0 );
-            set.Add( new Date { month = midsummer.Month, day = midsummer.Day } );
+            midsummer = AddDays(midsummer, wdDiff);
+            set.Add(midsummer);
+            midsummer = AddDays(midsummer, 1);
+            set.Add(midsummer);
 
-            var saintsDay = new DateTime( year, 10, 31 ).Date;
+            //All saints day
+            var saintsDay = new DateTime(year, 10, 31);
             wdDiff = (int)DayOfWeek.Saturday - (int)saintsDay.DayOfWeek;
-            if ( wdDiff < 0 )
+            if (wdDiff < 0)
                 wdDiff += 7;
-            saintsDay += new TimeSpan( wdDiff, 0, 0, 0 );
-            set.Add( new Date { month = saintsDay.Month, day = saintsDay.Day } );
+            set.Add(AddDays(saintsDay, wdDiff));
 
-            movingHolidays[year] = set;
+            //Fixed holidays
+            set.Add(new DateTime(year, 1, 1));
+            set.Add(new DateTime(year, 1, 6));
+            set.Add(new DateTime(year, 5, 1));
+            set.Add(new DateTime(year, 6, 6));
+            set.Add(new DateTime(year, 12, 24));
+            set.Add(new DateTime(year, 12, 25));
+            set.Add(new DateTime(year, 12, 26));
+            set.Add(new DateTime(year, 12, 31));
+
+            holidays[year] = set;
         }
 
         private void CalcCourseDays() {
             courseDays = new List<DateTime>();
-            for ( DateTime d = firstDate; d <= lastDate; d = d + new TimeSpan( 1, 0, 0, 0 ) ) {
-                if ( _IsCourseDay( d ) )
-                    courseDays.Add( d );
+            for (DateTime d = firstDate; d <= lastDate; d = AddDays(d, 1)) {
+                if (IsCourseDay_NoLimits(d))
+                    courseDays.Add(d);
             }
         }
 
-        private bool _IsCourseDay( DateTime d ) {
+        private bool IsHoliday( DateTime d) {
+            if (!holidays.ContainsKey(d.Year))
+                CalcHolidays(d.Year);
+            return holidays[d.Year].Contains(d);
+        }
 
-            if ( weekdays.Contains( d.DayOfWeek ) ) {
-                Date date = new Date { month = d.Month, day = d.Day };
-
-                if ( fixedHolidays.Contains( date ) )
-                    return false;
-
-                if ( !movingHolidays.ContainsKey( d.Year ) )
-                    CalcMovingHolidays( d.Year );
-                if ( movingHolidays[d.Year].Contains( date ) )
-                    return false;
-
-                return true;
-            }
-
+        private bool IsCourseDay_NoLimits( DateTime d ) {
+            if ( courseWeekdays.Contains( d.DayOfWeek ) )
+                return !IsHoliday(d);
             return false;
+        }
+
+        private DateTime AddDays(DateTime date, int days) {
+            return date + new TimeSpan(days, 0, 0, 0);
         }
 
         // 1/1 helg
