794a1fda93111c9809d18702a553c3b5bbd3215d
Lennart0 <lennart0@hotmail.se>
2016-07-04 09:24:26 +0200

    New FromScratch migration.

--- LMS/LMS/Migrations/201607040720173_FromScratch.Designer.cs
+++ LMS/LMS/Migrations/201607040720173_FromScratch.Designer.cs
@@ -7,13 +7,13 @@ namespace LMS.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class FromScratch : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(FromScratch));
         
         string IMigrationMetadata.Id
         {
-            get { return "201607011341171_init"; }
+            get { return "201607040720173_FromScratch"; }
         }
         
         string IMigrationMetadata.Source
